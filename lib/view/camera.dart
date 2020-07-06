714c7c346684d261a4f1bcf9c6ac06986dbca738
MyStic <mystic@MacBook-Pro.local>
2020-07-06 11:55:18 +0700

    Fix camera crash

--- lib/view/camera.dart
+++ lib/view/camera.dart
@@ -15,23 +15,28 @@ class CameraView extends StatefulWidget {
 
 class _CameraViewState extends State<CameraView> {
   CameraController _controller;
-  Future<void> _initializeControllerFuture;
+  // Future<void> _initializeControllerFuture;
   bool isCameraReady = false;
   bool _isDetecting = false;
   dynamic _scanResults;
-
+  CameraLensDirection _direction = CameraLensDirection.back;
   @override
   void initState() {
     super.initState();
     _initializeCamera();
   }
 
-  Future<void> _initializeCamera() async {
-    final cameras = await availableCameras();
-    final firstCamera = cameras.first;
-    CameraLensDirection _direction = CameraLensDirection.back;
-    _controller = CameraController(firstCamera, ResolutionPreset.high);
-    _initializeControllerFuture = _controller.initialize();
+  void _initializeCamera() async {
+    // final cameras = await availableCameras();
+    // final firstCamera = cameras.first;
+
+    CameraDescription description = await getCamera(_direction);
+    ImageRotation rotation = rotationIntToImageRotation(
+      description.sensorOrientation,
+    );
+
+    _controller = CameraController(description, ResolutionPreset.high);
+    await _controller.initialize();
     if (!mounted) {
       return;
     }
@@ -39,10 +44,6 @@ class _CameraViewState extends State<CameraView> {
       isCameraReady = true;
     });
 
-    CameraDescription description = await getCamera(_direction);
-    ImageRotation rotation = rotationIntToImageRotation(
-      description.sensorOrientation,
-    );
     _controller.startImageStream((CameraImage image) {
       if (_isDetecting) return;
       _isDetecting = true;
@@ -85,14 +86,14 @@ class _CameraViewState extends State<CameraView> {
     );
   }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      _controller != null
-          ? _initializeControllerFuture = _controller.initialize()
-          : null; //on pause camera is disposed, so we need to call again "issue is only for android"
-    }
-  }
+  // @override
+  // void didChangeAppLifecycleState(AppLifecycleState state) {
+  //   if (state == AppLifecycleState.resumed) {
+  //     _controller != null
+  //         ? _initializeControllerFuture = _controller.initialize()
+  //         : null; //on pause camera is disposed, so we need to call again "issue is only for android"
+  //   }
+  // }
 
   Widget _buildImage() {
     return Container(
@@ -119,60 +120,59 @@ class _CameraViewState extends State<CameraView> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final deviceRatio = size.width / size.height;
+    // final size = MediaQuery.of(context).size;
+    // final deviceRatio = size.width / size.height;
 
-    return FutureBuilder<void>(
-      future: _initializeControllerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          // If the Future is complete, display the preview.
-          return Scaffold(
-            body: _buildImage(),
-            // body: Transform.scale(
-            //   scale: _controller.value.aspectRatio / deviceRatio,
-            //   child: Center(
-            //     child: AspectRatio(
-            //       aspectRatio: _controller.value.aspectRatio,
-            //       child: CameraPreview(_controller), //cameraPreview
-            //     ),
-            //   ),
-            // ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.endDocked,
-            floatingActionButton: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 30,
-                ),
-                Opacity(
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      _controller.stopImageStream();
-                      _controller.dispose();
-                      Navigator.pop(context);
-                    },
-                    tooltip: 'Exit',
-                    child: Icon(
-                      Icons.cancel,
-                      size: 40,
-                    ),
-                    elevation: 0,
-                    backgroundColor: Colors.transparent,
-                  ),
-                  opacity: 0.5,
-                ),
-              ],
+    // return FutureBuilder<void>(
+    // future: _initializeControllerFuture,
+    // builder: (context, snapshot) {
+    // if (snapshot.connectionState == ConnectionState.done) {
+    // If the Future is complete, display the preview.
+    return Scaffold(
+      body: _buildImage(),
+      // body: Transform.scale(
+      //   scale: _controller.value.aspectRatio / deviceRatio,
+      //   child: Center(
+      //     child: AspectRatio(
+      //       aspectRatio: _controller.value.aspectRatio,
+      //       child: CameraPreview(_controller), //cameraPreview
+      //     ),
+      //   ),
+      // ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 30,
+          ),
+          Opacity(
+            child: FloatingActionButton(
+              onPressed: () async {
+                await _controller?.stopImageStream();
+                await _controller?.dispose();
+                Navigator.pop(context);
+              },
+              tooltip: 'Exit',
+              child: Icon(
+                Icons.cancel,
+                size: 40,
+              ),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
             ),
-          );
-        } else {
-          return Center(
-            child: CircularProgressIndicator(
-              backgroundColor: const Color(0xff1588db),
-            ),
-          ); // Otherwise, display a loading indicator.
-        }
-      },
+            opacity: 0.5,
+          ),
+        ],
+      ),
     );
+    // } else {
+    // return Center(
+    // child: CircularProgressIndicator(
+    // backgroundColor: const Color(0xff1588db),
+    // ),
+    // ); // Otherwise, display a loading indicator.
   }
+  // },
+  // );
 }
+// }
