faf182ca3f34a8750b4d16e6b4843ed6e94955d0
MyStic <mystic@MacBook-Pro.local>
2020-07-15 22:31:29 +0700

    Merge Nearby

--- lib/controller/config.dart
+++ lib/controller/config.dart
@@ -1,9 +1,5 @@
 // Các thiết lập chung: địa chỉ server...
 // final base_url = 'http://localhost:5000';
-<<<<<<< HEAD
 // final base_url = 'http://192.168.43.132:5000';
-=======
-final base_url = 'http://192.168.1.254:5000';
->>>>>>> long
 // final base_url = 'http://192.168.43.1:5000';
 final base_url = 'http://192.168.0.140:5000';
