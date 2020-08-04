2793ab388d2462c1114901e50b339c879212ba93
longbui <buingohoanglong2000@gmail.com>
2020-08-04 14:42:41 +0700

    share

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -414,6 +414,7 @@ class InformationCard extends StatelessWidget {
                   text: 'Share this contact to your friend',
                   onTap: () {
                     openEmailApp(context);
+                    // shareInfo(info, context);
                   },
                 ),
                 InfoMenuCard(
@@ -529,6 +530,44 @@ class InformationCard extends StatelessWidget {
 
   void makePhoneCall(String phoneNumber) {
     launch("tel://" + phoneNumber);
+  }
+
+  void shareInfo(MemberInfo info, BuildContext context) {
+    var emailController = new TextEditingController();
+    showDialog(
+        context: context,
+        child: Dialog(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: new InputDecoration(hintText: "Type email here..."),
+                controller: emailController,
+              ),
+              FlatButton(
+                  onPressed: () async {
+                    print(emailController.text);
+                    var body = {
+                      "recipient_email": emailController.text,
+                      "info": {
+                        "name": info.getName(),
+                        "id": info.getID(),
+                      }
+                    };
+                    var storage = FlutterSecureStorage();
+                    var token = await storage.read(key: "token");
+                    var header = {
+                      "Authorization": "Bearer $token",
+                      "Content-Type": "application/json"
+                    };
+                    final url = base_url + '/share/';
+                    var response = await http.post(url,
+                        headers: header, body: jsonEncode(body));
+                    print(response);
+                  },
+                  child: Text("Share"))
+            ],
+          ),
+        ));
   }
 
   void openEmailApp(BuildContext context) {
